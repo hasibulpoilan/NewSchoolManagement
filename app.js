aa84a2f152bb35acca265f2d35fb65509264e742
hasibulpoilan <hasibulpailan8@gmail.com>
2024-08-23 17:34:32 +0530

    comment

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.delete('/deleteSchool/:id', async (req, res) => {
 
 
 
-// Start the server
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
